e910661ec000f3ae3f1e9183004910a4a20d0aa4
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-10-08 23:07:03 +0900

    update on css and adding carousel

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,8 @@
-document.getElementById('myButton').addEventListener('click', function () {
-    alert('Button was clicked!');
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 100) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
 });
